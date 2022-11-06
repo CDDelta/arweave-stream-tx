4c74fa294539e5ee6ba108337e275b87e5313a17
CDDelta <me@cddelta.dev>
2022-11-06 03:54:37 +0000

    fix: inconsistent chunking behaviour Fixes various issues with chunking behaviour by implementing our own chunker.

---
+++ src/common/chunker.ts
@@ -0,0 +1,72 @@
+export class ChunkBuffer {
+  readonly buffers: Buffer[] = [];
+
+  get empty(): boolean {
+    return this.buffers.length === 0;
+  }
+
+  push(...buffers: Buffer[]) {
+    this.buffers.push(...buffers);
+  }
+
+  pop(expectedChunkSize: number): Buffer | null {
+    let totalBufferSize = 0;
+
+    for (const [i, chunk] of this.buffers.entries()) {
+      totalBufferSize += chunk.byteLength;
+
+      if (totalBufferSize >= expectedChunkSize) {
+        const chunkBuffers = this.buffers.splice(0, i);
+
+        if (totalBufferSize === expectedChunkSize) {
+          chunkBuffers.push(...this.buffers.splice(0, 1));
+        } else if (totalBufferSize > expectedChunkSize) {
+          const chunkOverflowAmount = totalBufferSize - expectedChunkSize;
+          const chunkWatermark = chunk.byteLength - chunkOverflowAmount;
+          const chunkBelowWatermark = chunk.slice(0, chunkWatermark);
+          const chunkOverflow = chunk.slice(chunkWatermark);
+
+          this.buffers[0] = chunkOverflow;
+          chunkBuffers.push(chunkBelowWatermark);
+        }
+
+        return Buffer.concat(chunkBuffers);
+      }
+    }
+
+    return null;
+  }
+
+  flush(): Buffer {
+    const remaining = Buffer.concat(this.buffers);
+    this.buffers.length = 0;
+    return remaining;
+  }
+}
+
+export interface ChunkerOptions {
+  flush: boolean;
+}
+
+export function chunker(expectedChunkSize: number, { flush }: ChunkerOptions) {
+  return async function* (stream: AsyncIterable<Buffer>): AsyncIterable<Buffer> {
+    const chunkBuffer = new ChunkBuffer();
+
+    for await (const chunk of stream) {
+      chunkBuffer.push(chunk);
+
+      while (true) {
+        const sizedChunk = chunkBuffer.pop(expectedChunkSize);
+        if (!sizedChunk) {
+          break;
+        }
+
+        yield sizedChunk;
+      }
+    }
+
+    if (flush) {
+      yield chunkBuffer.flush();
+    }
+  };
+}
